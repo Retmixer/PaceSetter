13c56fd68e05b5d903aca34843b0465ca0857b67
Retmixer <114287870+Retmixer@users.noreply.github.com>
2025-09-19 11:37:46 +0300

    Update JavaTask.java

--- Task_1/JavaTask.java
+++ Task_1/JavaTask.java
@@ -57,3 +57,40 @@ public class Main {
         System.out.println("Числа: " + li_2);
     }
 }
+
+//Стеком №2
+
+public class MyStack<T> {  
+    private T stackArray;  
+    private int top;  
+    public MyStack(int capacity) {  
+        stackArray = (T) new Object[capacity];  
+        top = -1;  
+    }  
+    public void push(T item) {  
+        if (top == stackArray.length - 1) {  
+            throw new IllegalStateException("Stack overflow");  
+        }  
+        stackArray[++top] = item;  
+    }  
+    public T pop() {  
+        if (top == -1) {  
+            throw new IllegalStateException("Stack underflow");  
+        }  
+        return stackArray[top--];  
+    }  
+    public T peek() {  
+        if (top == -1) {  
+            throw new IllegalStateException("Stack is empty");  
+        }  
+        return stackArray[top];  
+    }  
+    public boolean isEmpty() {  
+        return (top == -1);  
+    }  
+    public int size() {  
+        return (top + 1);  
+    }  
+}  
+
+
